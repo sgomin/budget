449bc5f94971481d6b16a177bfc1fda6b9f24420
sgomin <stan@gomin.me>
2017-05-06 20:02:40 +1000

    allowing functional buttons in amount

--- www/js/budget/operations.js
+++ www/js/budget/operations.js
@@ -43,7 +43,7 @@ function evalAmount(event)
 function onAmountKey(e)
 {
     // backspace, delete, home, end, left, right
-    if (e.which == 8 || e.which == 46 || e.which == 37 || e.which == 39 ||
+    if (e.which == 0 || e.which == 8 || e.which == 46 || e.which == 37 || e.which == 39 ||
         e.which == 35 || e.which == 36)
     {
         return true;
